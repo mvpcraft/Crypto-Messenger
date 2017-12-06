status-im/status-mobile

Added password validation for Restore Access flow #1224

src/status_im/ui/screens/accounts/recover/db.cljs
@@ -1,6 +1,9 @@
 (ns status-im.ui.screens.accounts.recover.db
-  (:require [cljs.spec.alpha :as s]
+  (:require [cljs.spec.alpha :as spec]
             status-im.utils.db))
 
-(s/def ::passphrase :global/not-empty-string)
-(s/def ::password :global/not-empty-string)
+(defn valid-length? [identity]
+  (> (count identity) 5))
+
+(spec/def ::passphrase :global/not-empty-string)
+(spec/def ::password  (spec/and :global/not-empty-string valid-length?))

mvpcraft/Crypto-Messenger

src/status_im/ui/screens/accounts/recover/db.cljs
@@ -1,6 +1,10 @@
 (ns status-im.ui.screens.accounts.recover.db
-  (:require [cljs.spec.alpha :as s]
+  (:require [cljs.spec.alpha :as spec]
+            [status-im.constants :as const]
             status-im.utils.db))
 
-(s/def ::passphrase :global/not-empty-string)
-(s/def ::password :global/not-empty-string)
+(defn valid-length? [password]
+  (>= (count password) const/min-password-length))
+
+(spec/def ::passphrase :global/not-empty-string)
+(spec/def ::password  (spec/and :global/not-empty-string valid-length?))