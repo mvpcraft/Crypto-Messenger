wesias7/status-react

improve navigation duration

src/status_im/ui/components/chat_icon/screen.cljs
@@ -16,11 +16,12 @@
     (first name)]])
 
 (defn chat-icon [photo-path {:keys [size accessibility-label]}]
-  (let [photo (if (s/starts-with? photo-path "contacts://")
-                (->> (s/replace photo-path #"contacts://" "")
-                     (keyword)
-                     (get resources/contacts))
-                {:uri photo-path})]
+  (if-let [photo (when photo-path
+                   (if (s/starts-with? photo-path "contacts://")
+                     (->> (s/replace photo-path #"contacts://" "")
+                          (keyword)
+                          (get resources/contacts))
+                     {:uri photo-path}))]
     [image {:source photo
             :style  (st/image-style size)
             :accessibility-label (or accessibility-label :chat-icon)}]))

mvpcraft/Crypto-Messenger

src/status_im/ui/components/chat_icon/screen.cljs
@@ -13,11 +13,12 @@
     (first name)]])
 
 (defn chat-icon [photo-path {:keys [size accessibility-label]}]
-  (let [photo (if (string/starts-with? photo-path "contacts://")
-                (->> (string/replace photo-path #"contacts://" "")
-                     (keyword)
-                     (get resources/contacts))
-                {:uri photo-path})]
+  (let [photo (when photo-path
+                (if (string/starts-with? photo-path "contacts://")
+                  (->> (string/replace photo-path #"contacts://" "")
+                       (keyword)
+                       (get resources/contacts))
+                  {:uri photo-path}))]
     [react/image {:source              photo
                   :style               (styles/image-style size)
                   :accessibility-label (or accessibility-label :chat-icon)}]))