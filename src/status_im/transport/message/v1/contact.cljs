jaytoday/status-react

handle multiple incoming messages at once

src/status_im/transport/message/v1/contact.cljs
@@ -22,9 +22,9 @@
                                             :topic      topic
                                             :message    message}]))]
       (handlers-macro/merge-fx cofx
-                               {:shh/add-new-sym-key {:web3       (get-in cofx [:db :web3])
-                                                      :sym-key    sym-key
-                                                      :on-success on-success}}
+                               {:shh/add-new-sym-keys [{:web3       (get-in cofx [:db :web3])
+                                                        :sym-key    sym-key
+                                                        :on-success on-success}]}
                                (protocol/init-chat chat-id topic)))))
 
 (defrecord ContactRequest [name profile-image address fcm-token]
@@ -39,8 +39,8 @@
                                             :topic      topic
                                             :message    this}]))]
       (handlers-macro/merge-fx cofx
-                               {:shh/get-new-sym-key {:web3       (:web3 db)
-                                                      :on-success on-success}}
+                               {:shh/get-new-sym-key [{:web3       (:web3 db)
+                                                       :on-success on-success}]}
                                (protocol/init-chat chat-id topic))))
   (receive [this chat-id signature {:keys [db] :as cofx}]
     (contacts/receive-contact-request signature this cofx)))

src/status_im/transport/message/v1/group_chat.cljs
@@ -25,16 +25,20 @@
                                       :payload     this}
                                      cofx)))
   (receive [this _ signature {:keys [db] :as cofx}]
-    (handlers-macro/merge-fx cofx
-                             {:shh/add-new-sym-key {:web3       (:web3 db)
-                                                    :sym-key    sym-key
-                                                    :on-success (fn [sym-key sym-key-id]
-                                                                  (re-frame/dispatch [:group/add-new-sym-key {:chat-id    chat-id
-                                                                                                              :signature  signature
-                                                                                                              :sym-key    sym-key
-                                                                                                              :sym-key-id sym-key-id
-                                                                                                              :message    message}]))}}
-                             (protocol/init-chat chat-id))))
+    (handlers-macro/merge-fx
+     cofx
+     {:shh/add-new-sym-keys
+      [{:web3       (:web3 db)
+        :sym-key    sym-key
+        :on-success (fn [sym-key sym-key-id]
+                      (re-frame/dispatch
+                       [:group/add-new-sym-key
+                        {:chat-id    chat-id
+                         :signature  signature
+                         :sym-key    sym-key
+                         :sym-key-id sym-key-id
+                         :message    message}]))}]}
+     (protocol/init-chat chat-id))))
 
 (defn- user-is-group-admin? [chat-id cofx]
   (= (get-in cofx [:db :chats chat-id :group-admin])
@@ -42,12 +46,14 @@
 
 (defn- send-new-group-key [message chat-id cofx]
   (when (user-is-group-admin? chat-id cofx)
-    {:shh/get-new-sym-key {:web3 (get-in cofx [:db :web3])
-                           :on-success (fn [sym-key sym-key-id]
-                                         (re-frame/dispatch [:group/send-new-sym-key {:chat-id    chat-id
-                                                                                      :sym-key    sym-key
-                                                                                      :sym-key-id sym-key-id
-                                                                                      :message    message}]))}}))
+    {:shh/get-new-sym-key [{:web3       (get-in cofx [:db :web3])
+                            :on-success (fn [sym-key sym-key-id]
+                                          (re-frame/dispatch
+                                           [:group/send-new-sym-key
+                                            {:chat-id    chat-id
+                                             :sym-key    sym-key
+                                             :sym-key-id sym-key-id
+                                             :message    message}]))}]}))
 
 (defn- prepare-system-message [admin-name added-participants removed-participants contacts]
   (let [added-participants-names   (map #(get-in contacts [% :name] %) added-participants)

src/status_im/utils/handlers_macro.cljs
@@ -7,17 +7,28 @@
     (assoc cofx :db db)
     cofx))
 
-(def ^:private tx-keys #{:data-store/tx :data-store/base-tx})
+(def ^:private mergable-keys
+  #{:data-store/tx :data-store/base-tx :chat-received-message/add-fx
+    :shh/add-new-sym-keys :call-jail :shh/get-new-sym-key})
 
 (defn safe-merge [fx new-fx]
   (if (:merging-fx-with-common-keys fx)
     fx
     (let [common-keys (set/intersection (into #{} (keys fx))
                                         (into #{} (keys new-fx)))]
-      (if (empty? (set/difference common-keys (conj tx-keys :db)))
-        (merge (apply dissoc fx tx-keys)
-               (apply dissoc new-fx tx-keys)
+      (if (empty? (set/difference common-keys (conj mergable-keys :db)))
+        (merge (apply dissoc fx mergable-keys)
+               (apply dissoc new-fx mergable-keys)
                (merge-with into
-                           (select-keys fx tx-keys)
-                           (select-keys new-fx tx-keys)))
+                           (select-keys fx mergable-keys)
+                           (select-keys new-fx mergable-keys)))
         {:merging-fx-with-common-keys common-keys}))))
+
+(defn merge-effects [{:keys [db] :as initial-cofx} handler args]
+  (reduce (fn [fx arg]
+            (let [temp-cofx (update-db initial-cofx fx)]
+              (safe-merge
+               fx
+               (handler arg temp-cofx))))
+          {:db db}
+          args))

mvpcraft/Crypto-Messenger

src/status_im/transport/message/v1/contact.cljs
@@ -22,9 +22,9 @@
                                             :topic      topic
                                             :message    message}]))]
       (handlers-macro/merge-fx cofx
-                               {:shh/add-new-sym-key {:web3       (get-in cofx [:db :web3])
-                                                      :sym-key    sym-key
-                                                      :on-success on-success}}
+                               {:shh/add-new-sym-keys [{:web3       (get-in cofx [:db :web3])
+                                                        :sym-key    sym-key
+                                                        :on-success on-success}]}
                                (protocol/init-chat chat-id topic)))))
 
 (defrecord ContactRequest [name profile-image address fcm-token]
@@ -39,8 +39,8 @@
                                             :topic      topic
                                             :message    this}]))]
       (handlers-macro/merge-fx cofx
-                               {:shh/get-new-sym-key {:web3       (:web3 db)
-                                                      :on-success on-success}}
+                               {:shh/get-new-sym-keys [{:web3       (:web3 db)
+                                                        :on-success on-success}]}
                                (protocol/init-chat chat-id topic))))
   (receive [this chat-id signature {:keys [db] :as cofx}]
     (contacts/receive-contact-request signature this cofx)))

src/status_im/transport/message/v1/group_chat.cljs
@@ -25,16 +25,20 @@
                                       :payload     this}
                                      cofx)))
   (receive [this _ signature {:keys [db] :as cofx}]
-    (handlers-macro/merge-fx cofx
-                             {:shh/add-new-sym-key {:web3       (:web3 db)
-                                                    :sym-key    sym-key
-                                                    :on-success (fn [sym-key sym-key-id]
-                                                                  (re-frame/dispatch [:group/add-new-sym-key {:chat-id    chat-id
-                                                                                                              :signature  signature
-                                                                                                              :sym-key    sym-key
-                                                                                                              :sym-key-id sym-key-id
-                                                                                                              :message    message}]))}}
-                             (protocol/init-chat chat-id))))
+    (handlers-macro/merge-fx
+     cofx
+     {:shh/add-new-sym-keys
+      [{:web3       (:web3 db)
+        :sym-key    sym-key
+        :on-success (fn [sym-key sym-key-id]
+                      (re-frame/dispatch
+                       [:group/add-new-sym-key
+                        {:chat-id    chat-id
+                         :signature  signature
+                         :sym-key    sym-key
+                         :sym-key-id sym-key-id
+                         :message    message}]))}]}
+     (protocol/init-chat chat-id))))
 
 (defn- user-is-group-admin? [chat-id cofx]
   (= (get-in cofx [:db :chats chat-id :group-admin])
@@ -42,12 +46,14 @@
 
 (defn- send-new-group-key [message chat-id cofx]
   (when (user-is-group-admin? chat-id cofx)
-    {:shh/get-new-sym-key {:web3 (get-in cofx [:db :web3])
-                           :on-success (fn [sym-key sym-key-id]
-                                         (re-frame/dispatch [:group/send-new-sym-key {:chat-id    chat-id
-                                                                                      :sym-key    sym-key
-                                                                                      :sym-key-id sym-key-id
-                                                                                      :message    message}]))}}))
+    {:shh/get-new-sym-keys [{:web3       (get-in cofx [:db :web3])
+                             :on-success (fn [sym-key sym-key-id]
+                                           (re-frame/dispatch
+                                            [:group/send-new-sym-key
+                                             {:chat-id    chat-id
+                                              :sym-key    sym-key
+                                              :sym-key-id sym-key-id
+                                              :message    message}]))}]}))
 
 (defn- prepare-system-message [admin-name added-participants removed-participants contacts]
   (let [added-participants-names   (map #(get-in contacts [% :name] %) added-participants)

src/status_im/utils/handlers_macro.cljs
@@ -7,17 +7,28 @@
     (assoc cofx :db db)
     cofx))
 
-(def ^:private tx-keys #{:data-store/tx :data-store/base-tx})
+(def ^:private mergable-keys
+  #{:data-store/tx :data-store/base-tx :chat-received-message/add-fx
+    :shh/add-new-sym-keys :call-jail :shh/get-new-sym-keys})
 
 (defn safe-merge [fx new-fx]
   (if (:merging-fx-with-common-keys fx)
     fx
     (let [common-keys (set/intersection (into #{} (keys fx))
                                         (into #{} (keys new-fx)))]
-      (if (empty? (set/difference common-keys (conj tx-keys :db)))
-        (merge (apply dissoc fx tx-keys)
-               (apply dissoc new-fx tx-keys)
+      (if (empty? (set/difference common-keys (conj mergable-keys :db)))
+        (merge (apply dissoc fx mergable-keys)
+               (apply dissoc new-fx mergable-keys)
                (merge-with into
-                           (select-keys fx tx-keys)
-                           (select-keys new-fx tx-keys)))
+                           (select-keys fx mergable-keys)
+                           (select-keys new-fx mergable-keys)))
         {:merging-fx-with-common-keys common-keys}))))
+
+(defn merge-effects [{:keys [db] :as initial-cofx} handler args]
+  (reduce (fn [fx arg]
+            (let [temp-cofx (update-db initial-cofx fx)]
+              (safe-merge
+               fx
+               (handler arg temp-cofx))))
+          {:db db}
+          args))