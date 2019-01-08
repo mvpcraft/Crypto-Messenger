srisankethu/status-react

Move group chats to their own topic
This commit moves group chats to their own topic, based on the randomly
generated chat-id. It falls back on the discovery topic for those peers
who we can't fingerprint the version, for backward compatibility.

src/status_im/data_store/realm/schemas/account/core.cljs
@@ -304,6 +304,19 @@
           browser/v8
           dapp-permissions/v9])
 
+(def v29 [chat/v11
+          transport/v7
+          contact/v3
+          message/v9
+          mailserver/v11
+          mailserver-topic/v1
+          user-status/v2
+          membership-update/v1
+          installation/v2
+          local-storage/v1
+          browser/v8
+          dapp-permissions/v9])
+
 ;; put schemas ordered by version
 (def schemas [{:schema        v1
                :schemaVersion 1
@@ -388,4 +401,7 @@
                :migration     migrations/v27}
               {:schema        v28
                :schemaVersion 28
-               :migration     migrations/v28}])
+               :migration     migrations/v28}
+              {:schema        v29
+               :schemaVersion 29
+               :migration     (constantly nil)}])

mvpcraft/Crypto-Messenger

src/status_im/data_store/realm/schemas/account/core.cljs
@@ -330,6 +330,19 @@
           browser/v8
           dapp-permissions/v9])
 
+(def v31 [chat/v13
+          transport/v7
+          contact/v3
+          message/v9
+          mailserver/v11
+          mailserver-topic/v1
+          user-status/v2
+          membership-update/v1
+          installation/v2
+          local-storage/v1
+          browser/v8
+          dapp-permissions/v9])
+
 ;; put schemas ordered by version
 (def schemas [{:schema        v1
                :schemaVersion 1
@@ -420,4 +433,7 @@
                :migration     migrations/v29}
               {:schema        v30
                :schemaVersion 30
-               :migration     migrations/v30}])
+               :migration     migrations/v30}
+              {:schema        v31
+               :schemaVersion 31
+               :migration     (constantly nil)}])