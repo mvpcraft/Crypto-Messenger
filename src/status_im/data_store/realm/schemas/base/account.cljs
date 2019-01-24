kevinmiles/status-react

Add device name to pairing

src/status_im/data_store/realm/schemas/base/account.cljs
@@ -217,3 +217,7 @@
 (def v16 (assoc-in v15
                    [:properties :keycard-instance-uid]
                    {:type :string :optional true}))
+
+(def v17 (assoc-in v16
+                   [:properties :installation-name]
+                   {:type :string :optional true}))

src/status_im/data_store/realm/schemas/base/core.cljs
@@ -71,6 +71,11 @@
           extension/v12
           account/v16])
 
+(def v19 [network/v1
+          bootnode/v4
+          extension/v12
+          account/v17])
+
 ;; put schemas ordered by version
 (def schemas [{:schema        v1
                :schemaVersion 1
@@ -125,4 +130,7 @@
                :migration     migrations/v17}
               {:schema        v18
                :schemaVersion 18
-               :migration     migrations/v18}])
+               :migration     migrations/v18}
+              {:schema        v19
+               :schemaVersion 19
+               :migration     (constantly nil)}])

mvpcraft/Crypto-Messenger

Signed-off-by: Andrea Maria Piana <andrea.maria.piana@gmail.com>

src/status_im/data_store/realm/schemas/base/account.cljs
@@ -222,3 +222,6 @@
                                         {:type :string :optional true}
                                         :keycard-paired-on
                                         {:type :int :optional true}}))
+(def v18 (assoc-in v17
+                   [:properties :installation-name]
+                   {:type :string :optional true}))

src/status_im/data_store/realm/schemas/base/core.cljs
@@ -81,6 +81,11 @@
           extension/v12
           account/v17])
 
+(def v22 [network/v1
+          bootnode/v4
+          extension/v12
+          account/v18])
+
 ;; put schemas ordered by version
 (def schemas [{:schema        v1
                :schemaVersion 1
@@ -144,4 +149,7 @@
                :migration     migrations/v20}
               {:schema        v21
                :schemaVersion 21
-               :migration     migrations/v21}])
+               :migration     migrations/v21}
+              {:schema        v22
+               :schemaVersion 22
+               :migration     (constantly nil)}])