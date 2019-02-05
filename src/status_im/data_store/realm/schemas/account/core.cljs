zhouribin/status-react

[refactor] contact model: remove unused fields and add `blocked?`
- dapp related fields are removed since it is not used anymore
- blocked? field is added for futur block user feature

src/status_im/data_store/realm/schemas/account/core.cljs
@@ -304,6 +304,19 @@
           browser/v8
           dapp-permissions/v9])
 
+(def v29 [chat/v10
+          transport/v7
+          contact/v4
+          message/v9
+          mailserver/v11
+          mailserver-topic/v1
+          user-status/v2
+          membership-update/v1
+          installation/v2
+          local-storage/v1
+          browser/v8
+          dapp-permissions/v9])
+
 ;; put schemas ordered by version
 (def schemas [{:schema        v1
                :schemaVersion 1
@@ -388,4 +401,7 @@
                :migration     migrations/v27}
               {:schema        v28
                :schemaVersion 28
-               :migration     migrations/v28}])
+               :migration     migrations/v28}
+              {:schema        v29
+               :schemaVersion 29
+               :migration     migrations/v29}])

mvpcraft/Crypto-Messenger

src/status_im/data_store/realm/schemas/account/core.cljs
@@ -399,6 +399,20 @@
           dapp-permissions/v9
           contact-recovery/v1])
 
+(def v36 [chat/v14
+          transport/v8
+          contact/v4
+          message/v9
+          mailserver/v11
+          mailserver-topic/v1
+          user-status/v2
+          membership-update/v1
+          installation/v3
+          local-storage/v1
+          browser/v8
+          dapp-permissions/v9
+          contact-recovery/v1])
+
 ;; put schemas ordered by version
 (def schemas [{:schema        v1
                :schemaVersion 1
@@ -504,4 +518,7 @@
                :migration     migrations/v34}
               {:schema        v35
                :schemaVersion 35
-               :migration     migrations/v35}])
+               :migration     migrations/v35}
+              {:schema        v36
+               :schemaVersion 36
+               :migration     (constantly nil)}])