bb2000/status-react

[fix] use icons from design components

src/status_im/ui/screens/main_tabs/views.cljs
@@ -15,29 +15,25 @@
 (def tabs-list-data
   [{:view-id             :home
     :content             {:title         (i18n/label :t/home)
-                          :icon-inactive :icons/home
-                          :icon-active   :icons/home-active}
+                          :icon :toolbar-icons/home}
     :count-subscription  :chats/unread-messages-number
     :accessibility-label :home-tab-button}
    {:view-id             :wallet
     :content             {:title         (i18n/label :t/wallet)
-                          :icon-inactive :icons/wallet
-                          :icon-active   :icons/wallet-active}
+                          :icon :toolbar-icons/wallet}
     :count-subscription  :get-wallet-unread-messages-number
     :accessibility-label :wallet-tab-button}
    {:view-id             :my-profile
     :content             {:title         (i18n/label :t/profile)
-                          :icon-inactive :icons/profile
-                          :icon-active   :icons/profile-active}
+                          :icon :toolbar-icons/profile}
     :count-subscription  :get-profile-unread-messages-number
     :accessibility-label :profile-tab-button}])
 
-(defn- tab-content [{:keys [title icon-active icon-inactive]}]
+(defn- tab-content [{:keys [title icon]}]
   (fn [active? count]
     [react/view {:style styles/tab-container}
-     (let [icon (if active? icon-active icon-inactive)]
-       [react/view
-        [vector-icons/icon icon (styles/tab-icon active?)]])
+     [react/view
+      [vector-icons/icon icon (styles/tab-icon active?)]]
      [react/view
       [react/text {:style (styles/tab-title active?)}
        title]]

mvpcraft/Crypto-Messenger

Signed-off-by: yenda <eric@status.im>

src/status_im/ui/screens/main_tabs/views.cljs
@@ -15,29 +15,25 @@
 (def tabs-list-data
   [{:view-id             :home
     :content             {:title         (i18n/label :t/home)
-                          :icon-inactive :icons/home
-                          :icon-active   :icons/home-active}
+                          :icon :main-icons/home}
     :count-subscription  :chats/unread-messages-number
     :accessibility-label :home-tab-button}
    {:view-id             :wallet
     :content             {:title         (i18n/label :t/wallet)
-                          :icon-inactive :icons/wallet
-                          :icon-active   :icons/wallet-active}
+                          :icon :main-icons/wallet}
     :count-subscription  :get-wallet-unread-messages-number
     :accessibility-label :wallet-tab-button}
    {:view-id             :my-profile
     :content             {:title         (i18n/label :t/profile)
-                          :icon-inactive :icons/profile
-                          :icon-active   :icons/profile-active}
+                          :icon :main-icons/profile}
     :count-subscription  :get-profile-unread-messages-number
     :accessibility-label :profile-tab-button}])
 
-(defn- tab-content [{:keys [title icon-active icon-inactive]}]
+(defn- tab-content [{:keys [title icon]}]
   (fn [active? count]
     [react/view {:style styles/tab-container}
-     (let [icon (if active? icon-active icon-inactive)]
-       [react/view
-        [vector-icons/icon icon (styles/tab-icon active?)]])
+     [react/view
+      [vector-icons/icon icon (styles/tab-icon active?)]]
      [react/view
       [react/text {:style (styles/tab-title active?)}
        title]]