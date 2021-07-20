tuyulman/status

nix: upgrade Go 1.14 to 1.16, gomobile to 2021-02-20
With upgrade to Go above `1.15` and Gomobile we now drop support for darwin/arm32 builds.
This means all devices like iPhone 5 or older.

For more details see:
https://tip.golang.org/doc/go1.15#darwin

Signed-off-by: Jakub Sokołowski <jakub@status.im>

nix/pkgs/go-maven-resolver/default.nix
@@ -1,8 +1,8 @@
-{ lib, buildGo114Package, fetchFromGitHub }:
+{ lib, buildGoPackage, fetchFromGitHub }:
 
 let
   inherit (lib) strings;
-in buildGo114Package rec {
+in buildGoPackage rec {
   pname = "go-maven-resolver";
   version = strings.substring 0 7 rev;
   owner = "status-im";

mvpcraft/Crypto-Messenger

nix/pkgs/go-maven-resolver/default.nix
@@ -1,8 +1,8 @@
-{ lib, buildGo114Module, fetchFromGitHub }:
+{ lib, buildGo116Package, fetchFromGitHub }:
 
 let
   inherit (lib) strings;
-in buildGo114Module rec {
+in buildGo116Module rec {
   pname = "go-maven-resolver";
   version = "v1.1.1";
 