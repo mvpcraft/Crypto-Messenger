status-im/status-mobile

Adjust identifiers page to do highlighting (#15752)

src/utils/worklets/identifiers_highlighting.cljs
@@ -1,0 +1,23 @@
+(ns utils.worklets.identifiers-highlighting)
+
+(def worklets (js/require "../src/js/worklets/identifiers_highlighting.js"))
+
+(defn background-style
+  [color progress]
+  (.backgroundStyle ^js worklets color progress))
+
+(defn opacity
+  [progress]
+  (.opacity ^js worklets progress))
+
+(defn ring-style
+  [progress]
+  (.ringStyle ^js worklets progress))
+
+(defn user-hash-style
+  [color progress]
+  (.userHashStyle ^js worklets color progress))
+
+(defn emoji-hash-style
+  [progress]
+  (.emojiHashStyle ^js worklets progress))

mvpcraft/Crypto-Messenger

src/utils/worklets/identifiers_highlighting.cljs
@@ -1,0 +1,31 @@
+(ns utils.worklets.identifiers-highlighting)
+
+(def worklets (js/require "../src/js/worklets/identifiers_highlighting.js"))
+
+(defn background
+  [color progress]
+  (.background ^js worklets color progress))
+
+(defn opacity
+  [progress]
+  (.opacity ^js worklets progress))
+
+(defn avatar-opacity
+  [progress]
+  (.avatarOpacity ^js worklets progress))
+
+(defn ring-opacity
+  [progress]
+  (.ringOpacity ^js worklets progress))
+
+(defn user-hash-color
+  [progress]
+  (.userHashColor ^js worklets progress))
+
+(defn user-hash-opacity
+  [progress]
+  (.userHashOpacity ^js worklets progress))
+
+(defn emoji-hash-style
+  [progress]
+  (.emojiHashStyle ^js worklets progress))