3d7da05b7547983d1abb5cbbe4247656215d0f87
Ulises Manuel <90291778+ulisesmac@users.noreply.github.com>
2024-11-27 16:05:56 -0600

    fix(messenger): List of users overlap composer input (#21641)
    * Fix mentions overlapped
    
    * Fix rendering blink

--- src/status_im/contexts/chat/messenger/composer/mentions/view.cljs
+++ src/status_im/contexts/chat/messenger/composer/mentions/view.cljs
@@ -10,37 +10,36 @@
     [status-im.contexts.chat.messenger.messages.constants :as messages.constants]
     [utils.re-frame :as rf]))
 
-(defn mention-item
-  [user]
-  [contact-list-item/contact-list-item
-   {:on-press #(rf/dispatch [:chat.ui/select-mention user])}
-   user])
+(defn- mention-item
+  [[user-key user]]
+  (with-meta
+    [contact-list-item/contact-list-item {:on-press #(rf/dispatch [:chat.ui/select-mention user])}
+     user]
+    {:key user-key}))
 
 (defn view
   [layout-height]
-  (let [suggestions                               (rf/sub [:chat/mention-suggestions])
-        suggestions?                              (seq suggestions)
-        theme                                     (quo.theme/use-theme)
-        opacity                                   (reanimated/use-shared-value (if suggestions? 1 0))
-        [suggestions-state set-suggestions-state] (rn/use-state suggestions)
-        top                                       (min constants/mentions-max-height
-                                                       (* (count suggestions-state) 56)
-                                                       (- @layout-height
-                                                          (+ (safe-area/get-top)
-                                                             messages.constants/top-bar-height
-                                                             5)))]
+  (let [suggestions             (rf/sub [:chat/mention-suggestions])
+        suggestions?            (seq suggestions)
+        theme                   (quo.theme/use-theme)
+        opacity                 (reanimated/use-shared-value (if suggestions? 1 0))
+        [suggestions-state
+         set-suggestions-state] (rn/use-state suggestions)
+        top                     (min constants/mentions-max-height
+                                     (* (count suggestions-state) 56)
+                                     (- @layout-height
+                                        (+ (safe-area/get-top)
+                                           messages.constants/top-bar-height
+                                           5)))]
     (rn/use-effect
      (fn []
        (if suggestions?
          (set-suggestions-state suggestions)
          (js/setTimeout #(set-suggestions-state suggestions) 300))
        (reanimated/animate opacity (if suggestions? 1 0)))
-     [suggestions?])
-    [reanimated/view
-     {:style (style/container opacity top theme)}
-     [rn/flat-list
-      {:keyboard-should-persist-taps :always
-       :data                         (vals suggestions-state)
-       :key-fn                       :key
-       :render-fn                    mention-item
-       :accessibility-label          :mentions-list}]]))
+     [suggestions])
+    [reanimated/view {:style (style/container opacity top theme)}
+     [rn/scroll-view
+      {:accessibility-label          :mentions-list
+       :keyboard-should-persist-taps :always}
+      (map mention-item suggestions-state)]]))
