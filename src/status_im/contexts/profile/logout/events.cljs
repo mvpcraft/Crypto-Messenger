contropist/status-react

commit e6f290c06aacc6174226d0c13c392e62a697820b

Remove jump to (#21634)

src/status_im/core.cljs
@@ -20,8 +20,6 @@
     [status-im.common.universal-links :as universal-links]
     [status-im.config :as config]
     [status-im.contexts.profile.push-notifications.events :as notifications]
-    [status-im.contexts.shell.jump-to.state :as shell.state]
-    [status-im.contexts.shell.jump-to.utils :as shell.utils]
     [status-im.feature-flags :as ff]
     [status-im.navigation.core :as navigation]
     status-im.contexts.wallet.signals
@@ -63,8 +61,7 @@
   (interceptors/register-global-interceptors)
 
   ;; Shell
-  (async-storage/get-item :selected-stack-id #(shell.utils/change-selected-stack-id % nil nil))
-  (async-storage/get-item :screen-height #(reset! shell.state/screen-height %))
+  (async-storage/get-item :selected-stack-id #(re-frame/dispatch [:shell/change-tab %]))
 
   (when config/quo-preview-enabled?
     (ff/load-flags))

src/status_im/events.cljs
@@ -35,6 +35,8 @@
     status-im.contexts.profile.settings.events
     status-im.contexts.settings.language-and-currency.events
     status-im.contexts.settings.wallet.saved-addresses.events
+    status-im.contexts.shell.effects
+    status-im.contexts.shell.events
     status-im.contexts.shell.qr-reader.events
     status-im.contexts.shell.share.events
     status-im.contexts.syncing.events

src/legacy/status_im/browser/core.cljs
@@ -306,13 +306,13 @@
       {:dispatch [:universal-links/handle-url normalized-url]}
       (if config/show-not-implemented-features?
         (rf/merge cofx
-                  {:db (assoc db
-                              :browser/options
-                              {:browser-id (:browser-id browser)}
-                              :browser/screen-id :browser)}
+                  {:db         (assoc db
+                                      :browser/options
+                                      {:browser-id (:browser-id browser)}
+                                      :browser/screen-id :browser)
+                   :dispatch-n [[:shell/change-tab :browser-stack]]}
                   (navigation/pop-to-root :shell-stack)
                   (chat.events/close-chat (:current-chat-id db))
-                  (navigation/change-tab :browser-stack)
                   (update-browser browser)
                   (resolve-url nil))
         {:linking/open-url url}))))
@@ -323,20 +323,20 @@
   [{:keys [db] :as cofx} browser-id]
   (let [browser (get-in db [:browser/browsers browser-id])]
     (rf/merge cofx
-              {:db (assoc db
-                          :browser/options
-                          {:browser-id browser-id}
-                          :browser/screen-id :browser)}
+              {:db         (assoc db
+                                  :browser/options
+                                  {:browser-id browser-id}
+                                  :browser/screen-id :browser)
+               :dispatch-n [[:shell/change-tab :browser-stack]]}
               (update-browser browser)
-              (navigation/change-tab :browser-stack)
               (resolve-url nil))))
 
 (rf/defn open-browser-tabs
   {:events [:browser.ui/open-browser-tabs]}
   [{:keys [db] :as cofx}]
   (rf/merge cofx
-            {:db (assoc db :browser/screen-id :browser-tabs)}
-            (navigation/change-tab :browser-stack)))
+            {:db         (assoc db :browser/screen-id :browser-tabs)
+             :dispatch-n [[:shell/change-tab :browser-stack]]}))
 
 (rf/defn web3-error-callback
   {:events [:browser.dapp/transaction-on-error]}

src/legacy/status_im/events.cljs
@@ -6,7 +6,6 @@
     legacy.status-im.browser.permissions
     legacy.status-im.chat.models.loading
     legacy.status-im.data-store.chats
-    legacy.status-im.data-store.switcher-cards
     legacy.status-im.data-store.visibility-status-updates
     legacy.status-im.fleet.core
     legacy.status-im.group-chats.core
@@ -43,7 +42,6 @@
     status-im.contexts.onboarding.events
     status-im.contexts.shell.activity-center.events
     status-im.contexts.shell.activity-center.notification.contact-requests.events
-    status-im.contexts.shell.jump-to.events
     [utils.i18n :as i18n]
     [utils.re-frame :as rf]))
 

src/legacy/status_im/group_chats/core.cljs
@@ -20,10 +20,9 @@
     (chat.events/pop-to-root-and-navigate-to-chat cofx chat-id nil)))
 
 (re-frame/reg-event-fx :chat-removed
- (fn [{:keys [db]} [response chat-id]]
+ (fn [{:keys [db]} [response]]
    {:db (dissoc db :current-chat-id)
-    :fx [[:dispatch [:shell/close-switcher-card chat-id]]
-         [:dispatch [:sanitize-messages-and-process-response response]]
+    :fx [[:dispatch [:sanitize-messages-and-process-response response]]
          [:dispatch [:pop-to-root :shell-stack]]
          [:activity-center.notifications/fetch-unread-count]]}))
 

src/legacy/status_im/qr_scanner/core.cljs
@@ -78,8 +78,8 @@
 (rf/defn handle-eip681
   [cofx data]
   (rf/merge cofx
-            {:dispatch [:wallet-legacy/parse-eip681-uri-and-resolve-ens data]}
-            (navigation/change-tab :wallet-stack)
+            {:dispatch-n [[:shell/change-tab :wallet-stack]
+                          [:wallet-legacy/parse-eip681-uri-and-resolve-ens data]]}
             (navigation/pop-to-root :shell-stack)))
 
 (rf/defn handle-local-pairing

src/quo/core.cljs
@@ -149,7 +149,6 @@
     quo.components.share.qr-code.view
     quo.components.share.share-qr-code.view
     quo.components.slideshow.slider-bar.view
-    quo.components.switchers.group-messaging-card.view
     quo.components.tabs.account-selector
     quo.components.tabs.segmented-tab
     quo.components.tabs.tabs.view
@@ -425,9 +424,6 @@
 
 ;;;; Slideshow
 (def slider-bar quo.components.slideshow.slider-bar.view/view)
-
-;;;; SWITCHER
-(def group-messaging-card quo.components.switchers.group-messaging-card.view/view)
 
 ;;;; Tabs
 (def tabs quo.components.tabs.tabs.view/view)

src/status_im/contexts/chat/events.cljs
@@ -203,7 +203,7 @@
   [{db :db :as cofx} chat-id animation]
   (rf/merge cofx
             (when-not (:current-chat-id db)
-              {:dispatch [(if animation :shell/navigate-to :navigate-to) :chat chat-id animation]})
+              {:dispatch [:navigate-to :chat {:animation animation}]})
             (close-chat chat-id)
             (force-close-chat chat-id)
             (fn [{:keys [db]}]
@@ -270,9 +270,7 @@
   {:events [:chat.ui/close-and-remove-chat]}
   [{:keys [db now] :as cofx} chat-id]
   (rf/merge cofx
-            {:effects/push-notifications-clear-message-notifications [chat-id]
-             :dispatch                                               [:shell/close-switcher-card
-                                                                      chat-id]}
+            {:effects/push-notifications-clear-message-notifications [chat-id]}
             (deactivate-chat chat-id)
             (offload-messages chat-id)))
 

mvpcraft/Crypto-Messenger

commit a860a8e935fcc1a87d6d018e4e09b1268d1a119d

fix(chats): Fix chats leaking to other profile (#21713)
Fixes a regression introduced in PR
https://github.com/status-im/status-mobile/pull/21634.

What's important is that the fix for chats leaking to other profiles was a
client-side bug, thus not coming from status-go, thus no private data was
leaking to other profiles. This PR keeps the existing workaround to memoize
chats in the home screen.

Fixes https://github.com/status-im/status-mobile/issues/21696

src/status_im/contexts/profile/logout/events.cljs
@@ -14,8 +14,11 @@
                   network/status network/expensive?
                   centralized-metrics/user-confirmed?]
     network-type :network/network-type
-    :as          _db}]
+    :as          db}]
   (assoc db/app-db
+         ;; We must carry over the current `:selected-stack-id`, otherwise the
+         ;; app will start with `:view-id` as nil.
+         :shell/selected-stack-id             (:shell/selected-stack-id db)
          :profile/logging-out?                true
          :centralized-metrics/user-confirmed? user-confirmed?
          :network/type                        network-type
@@ -44,4 +47,3 @@
          [:dispatch-later
           {:ms       100
            :dispatch [:profile.logout/reset-state]}]]}))
-

src/status_im/core.cljs
@@ -9,7 +9,6 @@
     [native-module.core :as native-module]
     [re-frame.core :as re-frame]
     [re-frame.interop :as interop]
-    [react-native.async-storage :as async-storage]
     [react-native.core :as rn]
     [react-native.platform :as platform]
     [react-native.shake :as react-native-shake]
@@ -59,9 +58,6 @@
   (react-native-shake/add-shake-listener #(re-frame/dispatch [:shake-event]))
   (universal-links/initialize)
   (interceptors/register-global-interceptors)
-
-  ;; Shell
-  (async-storage/get-item :selected-stack-id #(re-frame/dispatch [:shell/change-tab %]))
 
   (when config/quo-preview-enabled?
     (ff/load-flags))

src/status_im/events.cljs
@@ -62,6 +62,9 @@
   (rf/merge
    cofx
    {:db db/app-db
+    :effects.async-storage/get {:keys [:selected-stack-id]
+                                :cb   (fn [{:keys [selected-stack-id]}]
+                                        (rf/dispatch [:shell/change-tab selected-stack-id]))}
     :theme/init-theme nil
     :effects.network/listen-to-network-info nil
     :effects.biometric/get-supported-type nil